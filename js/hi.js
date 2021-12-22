b2dc0018ef03c596091b61601d1a93c089475215
David <example@gmail.com>
2021-12-22 18:53:54 +0300

    first commit

---
+++ js/hi.js
@@ -0,0 +1,16 @@
+alert('hello');
+
+// const result = confirm('you are here?');
+
+
+// const answer = prompt("Вам есть 18?", "нет");
+
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt ('Как ваше имя?');
+answers[1] = prompt ('Как ваша фамилия?');
+answers[2] = prompt ('Сколько вам лет?');
+
+document.write(answers);
---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log("string" * 9);
+
+const person = "Alex";
+
+const bool = true;
+
+
+
+let und;
+console.log(und);
+
+const obg = {
+    name: "John",
+    age: 25
+
+};
+
+console.log(obg.name);
+
+let arr =['plum.png','orange.png', 6, {}];
+console.log(arr[1]);
